1020760bd988b6b0cea0dadad630a37d6a5e83ee
koddart <jose_sousa@ufms.br>
2026-04-02 00:00:45 -0400

    expansãoDaMovimentação
    Nessa comit foi implementado:
    Mecânica de agachar, correr, e mecânica de movimentação dificultada em superfícies inclinadas.
    
    A mecânica de agachar está com uma fluidez fraca indesejada, sendo necessário analisar novamente mais pra frente.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,40 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    [Header("Movement")]
-    public float moveSpeed;
+    private float moveSpeed;
 
+    [Header("Movement")]
+    public float walkSpeed;
+    public float sprintSpeed;
     public float groundDrag;
 
+    [Header("Jump")]
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
 
+    [Header("Crouching")]
+    public float crouchSpeed;
+    public float crouchYScale;
+    private float startYScale;
+
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("Slope Handlig")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool existingSlope;
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     bool grounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.3f;
 
     public Transform orientation;
 
@@ -31,19 +48,42 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     Rigidbody rb;
 
+    public MovementState state;
+
+    private float crouchHeightDifference;
+    private CapsuleCollider col;
+    private float originalHeight;
+    private Vector3 originalCenter;
+
+    public enum MovementState
+    {
+        walking,
+        sprinting,
+        crouching,
+        air
+    }
+
     void Start()
     {
+        col = GetComponentInChildren<CapsuleCollider>();
+        originalHeight = col.height;
+        originalCenter = col.center;
+
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         readyToJump = true;
+
+        startYScale = transform.localScale.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 1.5f + 0.3f, whatIsGround);
+        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
 
         MyInput();
+        SpeedControl();
+        StateHandler();
 
         if (grounded)
         {
@@ -60,17 +100,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
-        SpeedControl();
-
+        
         if (!grounded)
         {
             rb.AddForce(Vector3.down * 20f, ForceMode.Acceleration);
         }
-        if (rb.linearVelocity.y < 0)
-        {
-            rb.AddForce(Vector3.down * 25f, ForceMode.Acceleration);
-        }
+        //if (rb.linearVelocity.y < 0)
+        //{
+        //    rb.AddForce(Vector3.down * 25f, ForceMode.Acceleration);
+        //}
     }
+    
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
@@ -84,32 +124,90 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
             Invoke(nameof(ResetJump), jumpCooldown);
         }
+
+        if(Input.GetKeyDown(crouchKey))
+        {
+            col.height = crouchYScale;
+            col.center = new Vector3(0, crouchYScale / 2f, 0);
+        }
+
+        if (Input.GetKeyUp(crouchKey))
+        {
+            col.height = originalHeight;
+            col.center = originalCenter;
+        }
     }
+
+    private void StateHandler()
+{
+    if (Input.GetKey(crouchKey))
+    {
+        state = MovementState.crouching;
+        moveSpeed = crouchSpeed;
+    }
+
+    if(grounded && Input.GetKey(sprintKey))
+    {
+        state = MovementState.sprinting;
+        moveSpeed = sprintSpeed;
+    }
+
+    else if (grounded)
+    {
+        state = MovementState.walking;
+        moveSpeed = walkSpeed;
+    }
+
+    else
+    {
+        state = MovementState.air;
+    }
+}
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (OnSlope() && !existingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * moveSpeed * 20f, ForceMode.Force);
+
+            if (rb.linearVelocity.y > 0)
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+        }
 
         if(grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
 
         else if(!grounded)
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Acceleration);
+
+        rb.useGravity = !OnSlope();
     }
 
     private void SpeedControl()
     {
-        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-
-        if(flatVel.magnitude > moveSpeed)
+        if (OnSlope() && !existingSlope)
         {
-            Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            if(rb.linearVelocity.magnitude > moveSpeed)
+                rb.linearVelocity = rb.linearVelocity.normalized * moveSpeed;
+        }
+
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            if(flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
         }
     }
 
     private void Jump()
     {
+        existingSlope = true;
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
@@ -117,5 +215,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void ResetJump()
     {
         readyToJump = true;
+        existingSlope = false;
+    }
+    private bool OnSlope()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+
+        return false;
+    }
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
     }
 }
+
